bd37796891402311bef1fe91492bd9f4b7c02c8c
Bhaskar subhash <73716329+Iamgodop@users.noreply.github.com>
2024-02-04 10:38:08 +0530

    Add files via upload

---
+++ weather_api.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+
+void main()
+{
+	char api[200], city[50], *buffer, file_content[500];
+
+	input: printf("Enter city name: ");
+	gets(city);
+
+	sprintf(api, "curl \"https://api.openweathermap.org/data/2.5/weather?q=%s&appid=abe3a0f4d0b6cebfbe7393b4b4e3aa28&units=metric\" > weather.txt", city);
+	system(api);
+
+	FILE *fp_weather = fopen("weather.txt", "r");
+	fgets(file_content, 500, fp_weather);
+
+	if (! strcmp(file_content, "{\"cod\":\"404\",\"message\":\"city not found\"}"))
+	{
+		printf("%s: city not found!\n", city);
+		goto input;
+	}
+
+	else
+	{
+		buffer = strtok(file_content, "\"");
+		while (strcmp(buffer, "temp"))
+		{
+			buffer = strtok(NULL, "\"");
+		}
+		buffer = strtok(NULL, "\"");
+		buffer[0] = buffer[strlen(buffer) - 1] = ' ';
+		printf("Temperature in %s is%sC", city, buffer);	
+	}
+}
